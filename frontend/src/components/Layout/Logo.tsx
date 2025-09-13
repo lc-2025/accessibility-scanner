a854a9ae642fa7d260700d37a35778a8123ceeac
the-speaker3 <the_speaker3@protonmail.com>
2025-09-13 18:42:22 +0200

    chore: Comment specification

--- frontend/src/components/Layout/Logo.tsx
+++ frontend/src/components/Layout/Logo.tsx
@@ -8,6 +8,7 @@ import '../../css/components/Logo.css';
 function Logo() {
   return (
     <aside className="logo relative hidden h-auto w-full max-w-[248px] min-w-[248px] overflow-hidden bg-size-[100%] bg-left-top bg-no-repeat pb-[3rem] sm:inline-block">
+      {/* Image-Replacement */}
       <h6 className="logo__title text-default absolute right-full">
         Accessibility Scanner by AccessiWay
       </h6>
