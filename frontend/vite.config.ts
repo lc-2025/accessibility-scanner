95abd7898b6d439bdaac1a2dd38c39531859d00f
the-speaker3 <the_speaker3@protonmail.com>
2025-09-13 09:30:07 +0200

    chore: Testing resources setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
@@ -6,4 +7,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    clearMocks: true,
+    coverage: {
+      enabled: true,
+      include: ['./src/**/*.ts']
+    },
+    environment: 'happy-dom',
+    setupFiles:'./vite.setup.ts'
+  },
 })
