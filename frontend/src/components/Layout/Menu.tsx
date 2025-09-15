57d8ea0e1ab2338968b44593ecc5741469927932
the-speaker3 <the_speaker3@protonmail.com>
2025-09-15 12:41:46 +0200

    chore: Menu UX update

--- frontend/src/components/Layout/Menu.tsx
+++ frontend/src/components/Layout/Menu.tsx
@@ -1,4 +1,5 @@
-import { NavLink } from 'react-router';
+import { NavLink, useLocation } from 'react-router';
+import { useTranslation } from 'react-i18next';
 import { ROUTE } from '../../utils/constants';
 
 /**
@@ -7,18 +8,26 @@ import { ROUTE } from '../../utils/constants';
  * @returns {*}
  */
 function Menu() {
-  const { HOME } = ROUTE;
+  const { HOME, SCAN } = ROUTE;
+  const { t } = useTranslation();
+  const { pathname } = useLocation();
+
+  const getLabel = (path: string): string =>
+    path === HOME.PATH ? 'Home' : t('menu.scans');
 
   return (
     <nav className="menu text-default uppercase">
       <h6 className="menu__title hidden">Main Menu</h6>
-      <NavLink
-        className="menu__link hover:text-primary py-4 transition-colors hover:underline sm:mr-4 sm:ml-4 sm:pr-4 sm:pl-4"
-        title="Home"
-        to={HOME.PATH}
-      >
-        Home
-      </NavLink>
+      {[HOME, SCAN.LIST].map(({ PATH }, i) => (
+        <NavLink
+          className={({isActive}) => `menu__link hover:text-primary py-4 transition-colors hover:underline sm:mr-4 sm:ml-4 sm:pr-4 sm:pl-4 ${isActive && 'text-primary pointer-events-none'}`}
+          key={crypto.randomUUID() + i}
+          title={getLabel(PATH)}
+          to={PATH}
+        >
+          {getLabel(PATH)}
+        </NavLink>
+      ))}
     </nav>
   );
 }
